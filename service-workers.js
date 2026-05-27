4168fdc40ee6ef916ba4b6a6d0d2c675916927ed
Rohit Vishwakarma <rv4173167@gmail.com>
2026-05-28 01:29:24 +0530

    Update service worker cache and fetch handling

--- service-workers.js
+++ service-workers.js
@@ -1,23 +1,67 @@
-const CACHE_NAME = "death-note-cache-v12.8";
+const CACHE_NAME = "death-note-v13";
+
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js"
+  "/Death-Note/",
+  "/Death-Note/index.html",
+  "/Death-Note/style.css",
+  "/Death-Note/script.js",
+  "/Death-Note/manifest.json"
 ];
 
-// Install
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+
 });
 
-// Fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
   );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, clone));
+
+        return response;
+
+      })
+
+      .catch(() => caches.match(event.request))
+
+  );
+
 });
